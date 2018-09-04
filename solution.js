3e8d5f1fbb8eaff17f8201d84488c9ae909968fb
Dody Herawan <dody@Dodys-MacBook-Pro.local>
2018-09-04 22:16:00 +0700

    Release 0

--- solution.js
+++ solution.js
@@ -1,3 +1,4 @@
+/*
 //RELEASE 0
 function linearSearch(target, arr) {
     return arr.indexOf(target);
@@ -6,3 +7,20 @@ function linearSearch(target, arr) {
 let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 console.log(linearSearch(18, random_numbers)); //2
 console.log(linearSearch(9, random_numbers)); //1
+*/
+
+
+//RELEASE 1
+
+function globalLinearSearch(target, arr) {
+    var result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+let banana_arr = 'banana'.split('');
+console.log(globalLinearSearch('a', banana_arr))
